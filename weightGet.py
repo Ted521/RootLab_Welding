786cd25f0b6f1313f49ee78891f763f177d7925a
qls8044 <gpqls9628@gmail.com>
2023-03-30 19:39:44 +0900

    c mod

--- weightGet.py
+++ weightGet.py
@@ -1,8 +1,16 @@
 import wget
+import os
 
 def download(url, out_path='.'):
     wget.download(url, out=out_path)
 
+def pathCheck(path):
+    if not os.isdir(path):
+        os.makedirs(path)
+
 if __name__ == "__main__":
     url = "https://github.com/Ted521/RootLab_Welding/releases/download/weightFile/latest.pth"
-    download(url, out_path="./RootLab_Welding/data/work/latest.pth")
+    path = "./RootLab_Welding/data/work"
+    pathCheck(path)
+    fname = "./RootLab_Welding/data/work/latest.pth"
+    download(url, out_path=fname)
